55087a8ba4bc561bb96a61193a771b1cb4f3840a
JT Wheeler <jtlwheeler@gmail.com>
2021-01-09 19:54:35 -0500

    Query to get transactions using MediatR.

---
+++ Banking.Tests/QueryProcessor/Queries/Handlers/TransactionQueryHandlerTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Banking.QueryProcessor.Domain.BankAccount;
+using Banking.QueryProcessor.Queries.Handlers;
+using Banking.QueryProcessor.Queries.Queries;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Banking.Tests.QueryProcessor.Queries.Handlers
+{
+    public class TransactionQueryHandlerTests
+    {
+        [Fact]
+        public async void ShouldReturnTransactionsForBankAccount()
+        {
+            var query = new TransactionQuery("ABankAccountId");
+
+            var bankAccount = new BankAccount();
+            var transaction1 = new Transaction
+            {
+                Amount = 12.34m,
+                Type = TransactionType.Deposit,
+                Id = "1",
+                Description = "Description 1"
+            };
+
+            var transaction2 = new Transaction
+            {
+                Amount = 99.99m,
+                Type = TransactionType.Deposit,
+                Id = "2",
+                Description = "Description 2"
+            };
+
+            var transaction3 = new Transaction
+            {
+                Amount = 5663.99m,
+                Type = TransactionType.Deposit,
+                Id = "3",
+                Description = "Description 3"
+            };
+
+            bankAccount.Transactions.Add(transaction1);
+            bankAccount.Transactions.Add(transaction2);
+            bankAccount.Transactions.Add(transaction3);
+            
+            var bankAccountRepository = new Mock<IBankAccountRepository>();
+            bankAccountRepository
+                .Setup(mock => mock.Get(query.BankAccountId))
+                .Returns(Task.Run(() => bankAccount));
+
+            var handler = new TransactionQueryHandler(bankAccountRepository.Object);
+
+            var actualTransactions = await handler.Handle(query, new CancellationToken());
+
+            actualTransactions.Should().ContainInOrder(transaction1, transaction2, transaction3);
+        }
+    }
+}
---
+++ Banking/QueryProcessor/Queries/Handlers/TransactionQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Banking.QueryProcessor.Domain.BankAccount;
+using MediatR;
+
+namespace Banking.QueryProcessor.Queries.Handlers
+{
+    public class TransactionQuery : IRequest<List<Transaction>>
+    {
+        public string BankAccountId { get; private set; }
+
+        public TransactionQuery(string bankAccountId)
+        {
+            BankAccountId = bankAccountId;
+        }
+    }
+
+    public class TransactionQueryHandler: IRequestHandler<TransactionQuery, List<Transaction>>
+    {
+        private readonly IBankAccountRepository _bankAccountRepository;
+
+        public TransactionQueryHandler(IBankAccountRepository bankAccountRepository)
+        {
+            _bankAccountRepository = bankAccountRepository;
+        }
+
+        public async Task<List<Transaction>> Handle(TransactionQuery request, CancellationToken cancellationToken)
+        {
+            var bankAccount = await _bankAccountRepository.Get(request.BankAccountId);
+            return bankAccount.Transactions;
+        }
+    }
+}
